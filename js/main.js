a20c22c2b7b4c447564a8afaef38537ace225cef
Emilia <kaem24au@student.ju.se>
2025-02-20 16:27:55 +0100

    Added interactive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const logo = document.getElementById ("logo");
+logo.addEventListener ("mouseover", function () {
+    this.style.color = "rgb(161, 19, 123)";
+});
+logo.addEventListener ("mouseout", function () {
+    this.style.color = "black";
+});
